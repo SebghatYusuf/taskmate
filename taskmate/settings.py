f43143cc3e6f7b40162d31f4829549db26180f08
Sebghat Yusuf <sebghatyusuf@gmail.com>
2020-04-12 13:40:01 +0430

    Updated Req

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -116,6 +116,6 @@ LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 django_heroku.settings(locals())
