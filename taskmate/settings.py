9e10164e995e6b0777d145c77948322a68af1edf
Sebghat Yusuf <sebghatyusuf@gmail.com>
2020-04-12 13:37:09 +0430

    Some changes

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,6 +1,8 @@
 
 import os
 import environ
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -114,3 +116,6 @@ LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+django_heroku.settings(locals())
