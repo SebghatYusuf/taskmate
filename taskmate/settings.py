4e49f245266bd4f5d4d61ed3cebec0f00495b846
Sebghat Yusuf <sebghatyusuf@gmail.com>
2020-07-15 12:43:27 +0430

    back changes

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -120,7 +120,7 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
